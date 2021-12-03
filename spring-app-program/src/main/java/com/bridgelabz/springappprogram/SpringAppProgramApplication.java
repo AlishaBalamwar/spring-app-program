731a4587daa34eaba3b0d0f89f0c342736c5f79b
AlishaBalamwar <alishabalamwar7806@gmail.com>
2021-12-03 18:59:04 +0530

    Added: intial commit

---
+++ spring-app-program/src/main/java/com/bridgelabz/springappprogram/SpringAppProgramApplication.java
@@ -0,0 +1,13 @@
+package com.bridgelabz.springappprogram;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAppProgramApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringAppProgramApplication.class, args);
+	}
+
+}
---
+++ spring-app-program/src/test/java/com/bridgelabz/springappprogram/SpringAppProgramApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bridgelabz.springappprogram;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAppProgramApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
